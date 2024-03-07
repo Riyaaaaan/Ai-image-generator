7ece81d69dd5e89378bc1fd0b21768bd42a1f773
Riyaaaaan <riyaaan.mhd@gmail.com>
2024-03-07 22:50:58 +0530

    Updated

--- lib/api/api_const.dart
+++ lib/api/api_const.dart
@@ -1,2 +1,2 @@
-const String apiKey = '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';
+const String apiKey = 'API_KEY_HERE';
 const String baseUrl = 'https://api.openai.com/v1/images/generations';
